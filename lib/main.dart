74829ac6409f3ecaa403a0ef5149d647f9becbbc
Amie Croteau <ac04400@ravens.racc.edu>
2020-02-22 23:43:39 -0500

    added errors

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp( MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('Aidly'),
+      title: Text('Volunteer Today'),
       centerTitle: true,
+      backgroundColor: Colors.pink,
 
     ),
   body: Center(
-      child: Text('Volunteer Today')),
+      child: Text(
+        'Aidly',
+        style:TextStyle(
+          fontSize:80.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+
+
+        ),
+      ),
+  ),
+
     floatingActionButton:
     FloatingActionButton(
+      onPressed:() {},
+      backgroundColor: Colors.pink,
       child: Text('+'),
+
     ),
   ),
 
